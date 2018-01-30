17bfa760e3c420f89990efe9ff4222201acadcb2
dmitrysbn <dmitry.sbn@gmail.com>
2018-01-30 13:19:01 -0500

    Finish Stretch Challenge

--- exercise2.rb
+++ exercise2.rb
@@ -25,6 +25,15 @@ elsif drama_rating >= 4
 elsif comedy_rating >= 4
   puts "'#{comedy}' is hilarious."
 
+elsif (documentary_rating > drama_rating) && (documentary_rating > comedy_rating)
+  puts "I think you would really enjoy '#{documentary}'. It's a great documentary by Lixin Fan."
+
+elsif (drama_rating > documentary_rating) && (drama_rating > comedy_rating)
+  puts "Check out '#{drama}'. It won the 2015 Best Picture!"
+
+elsif (comedy_rating > documentary_rating) && (comedy_rating > drama_rating)
+  puts "'#{comedy}' is hilarious."
+
 else
   puts "Check out '#{book}' by Sun Tzu."
 
